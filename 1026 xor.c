1011dde320b30c2871ee353c4f063f06538790d1
manuh <emanuelcasella224@gmail.com>
2024-07-25 20:47:04 -0300

    1026 xor.c

--- 1026 xor.c
+++ 1026 xor.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(){
+    long long int a, b;
+    // Leia os pares de números até o final do arquivo (EOF) = faz rodar pra sempre
+    while(scanf("%lld %lld", &a, &b) != EOF){ 
+    // Calcule a soma no modo "Mofiz" usando a operação XOR (^)
+        printf("%lld\n", a ^ b);
+    }
+
+    return 0;
+}
